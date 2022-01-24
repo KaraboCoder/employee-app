1678c452db54f70f52edcc2e88362d3c0fcded76
Karabo Ngwato <karabocoder@gmail.com>
2022-01-25 00:07:02 +0200

    updated comments

--- employee-app-backend/src/utils/id-generator.ts
+++ employee-app-backend/src/utils/id-generator.ts
@@ -1,3 +1,7 @@
+/**
+ * Generates a random id
+ * Converts random numbers between 65 and 90 to ascii characters (Uppercase letters) to find the first two chars of the id
+ */
 export const generateId = (): string => {
     return String.fromCharCode(Math.ceil(Math.random() * (90 - 65) + 65)) + String.fromCharCode(Math.ceil(Math.random() * (90 - 65) + 65)) + Math.random().toString().substr(2, 4)
 }
